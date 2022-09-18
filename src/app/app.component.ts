7bc404b01a293cefc68fb0e74a829bb984d35361
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-18 20:29:44 +0300

    :octocat:

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Londra').subscribe({
+      next: (response) => {
+        console.log(response);
+
+      }
+    })
+  }
+}
