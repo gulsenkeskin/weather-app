8cdaa956961ed967d47c2fd7a2c765ac7ba77904
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-19 09:51:13 +0300

    added search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,21 @@ import { WeatherService } from './services/weather.service';
 export class AppComponent implements OnInit {
 
   constructor(private weatherService: WeatherService) { }
+  cityName: string = 'Londra';
 
   weatherData?: WeatherData;
 
   ngOnInit(): void {
-    this.weatherService.getWeatherData('Londra').subscribe({
+    this.onSubmit();
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = "";
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
       next: (response: WeatherData) => {
         this.weatherData = response;
         console.log(response);
